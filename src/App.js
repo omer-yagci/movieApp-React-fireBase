5481de7434d4d0883768c2a4f610d952c03ca1c1
khan <omeryagci3295@gmail.com>
2022-07-17 21:28:31 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Navbar from "./components/Navbar";
+import AuthContext from "./context/AuthContext";
+import Router from "./router/Router";
+import "./scss/App.scss";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router />
+    </>
+  );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { NavLink, Link, useNavigate } from "react-router-dom";
+import navbarStyles from "../components/navbar.module.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className={navbarStyles.navbar}>
+      <Link to="/" className={navbarStyles.title}>
+        <h3>React Movie App</h3>
+      </Link>
+      <div className={navbarStyles["link-container"]}>
+        <ul>
+          <li>
+            <NavLink className={navbarStyles.btn} to="/login">
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={navbarStyles.btn} to="/register">
+              Register
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
