7eaddf5f6bff80931cdc6c00e52b64e59ab10979
khan <omeryagci3295@gmail.com>
2022-07-17 22:59:10 +0300

    login page and styles added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import loginStyles from "../login/login.module.scss";
+import registerImage from "../../assests/register.jpg";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className={loginStyles.container}>
+      <section className={loginStyles.left}>
+        <img src={registerImage} alt="registerImage" />
+      </section>
+      <section className={loginStyles.right}>
+        <h1>Login</h1>
+
+        <form className={loginStyles["form-container"]}>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" />
+
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" />
+
+          <a href="/register">Forgot Your Password?</a>
+        </form>
+        <button className={loginStyles.btn}>Login</button>
+        <button className={loginStyles.btn}>Continie with Google</button>
+      </section>
+    </div>
+  );
 };
 
 export default Login;
