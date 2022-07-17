1cd9ba6afd0fe6e81056a09d33cdc653ceaba46d
khan <omeryagci3295@gmail.com>
2022-07-17 20:39:18 +0300

    Router,routes added

---
+++ src/pages/movedetail/MoveDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MoveDetail = () => {
+  return <div>MoveDetail</div>;
+};
+
+export default MoveDetail;
---
+++ src/router/Router.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Login from "../pages/login/Login";
+import Main from "../pages/main/Main";
+import MoveDetail from "../pages/movedetail/MoveDetail";
+import Register from "../pages/register/Register";
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="details" element={<MoveDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
