59b38e0cb5da487923328f6ee00c043d6c6db802
khan <omeryagci3295@gmail.com>
2022-07-20 16:11:15 +0300

    login pages shanges

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -14,6 +14,7 @@ const Login = () => {
     useMovieContext();
 
   const loginSubmitHandler = (event) => {
+    // console.log(password, email);
     event.preventDefault();
     if (password.length <= 0 || email.length <= 0) {
       toast.warn(
